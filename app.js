c37f90d57c9fe55b5a8d0f0f8401922447117635
Jodie Ly <jodie.ly@gmail.com>
2017-03-06 18:32:54 -0500

    console logging verb and route

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 
+app.use(function (req, res, next) {
+
+    console.log(req.method);
+    
+    // console.log()
+
+    next();
+})
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
+app.get('/news', function (req, res) {
+  res.send('This is the news')
+});
 
 app.listen(3000, function() {
   console.log('This app listening on port 3000!');
