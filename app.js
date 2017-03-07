2ff156fc4243d833229f04c06961e845a833f14c
Stefanie Sulzer <stefanie.sulzer@gmail.com>
2017-03-07 11:42:22 -0500

    we can send a rendeder html

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require( 'express' );
-const app = express(); 
+const app = express();
 const chalk = require('chalk');
 const request = chalk.blue;
 const response = chalk.green;
 const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
 
+var data = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+}; // our objects for the nunjuck rendering.
+
+nunjucks.configure('views', {noCache: true}); //  needs to slash before view
+nunjucks.render('index.html', data, function(err, output) {
+  if (err) throw err;
+  console.log(output);
+} );
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 
 app.use(volleyball);
 
@@ -23,8 +41,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
-app.get('/news', function (req, res) {
-  res.send('This is the news')
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+const fillIns = {title: 'Hall of Fame', people: people};
+app.get('/halloffame', function (req, res) {
+  res.render('index.html', fillIns);
 });
 
 
