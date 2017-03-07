06355e382d4a8d8203f49fc6a8b350a6e22ecf3e
Stefanie Sulzer <stefanie.sulzer@gmail.com>
2017-03-07 15:09:47 -0500

    we have a twitter pagegit add .112

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,18 @@ const express = require('express');
 const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
+const path = require('path'); // WHAT IS THIS? Don't need to know?
+
+router.use(express.static('public'));
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets } );
 });
+
+// router.get('/stylesheets/style.css', function(req, res) {
+//   // res.sendFile(path.join(__dirname, '../public/stylesheets/style.css')); // works
+//   res.sendFile(path.join(__dirname, '../public/stylesheets', 'style.css')); // also works.
+// });
 
 module.exports = router;
