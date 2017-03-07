b7e169ad452aea85e2b4676b5a18b7ba1600285f
Jodie Ly <jodie.ly@gmail.com>
2017-03-07 14:34:22 -0500

    routing

--- app.js
+++ app.js
@@ -5,21 +5,24 @@ const request = chalk.blue;
 const response = chalk.green;
 const volleyball = require('volleyball');
 const nunjucks = require('nunjucks');
+const routes = require('./routes');
 
-var data = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-}; // our objects for the nunjuck rendering.
+app.use('/', routes);
+
+// var data = {
+//     title: 'An Example',
+//     people: [
+//         { name: 'Gandalf'},
+//         { name: 'Frodo' },
+//         { name: 'Hermione'}
+//     ]
+// }; // our objects for the nunjuck rendering.
 
 nunjucks.configure('views', {noCache: true}); //  needs to slash before view
-nunjucks.render('index.html', data, function(err, output) {
-  if (err) throw err;
-  console.log(output);
-} );
+// nunjucks.render('index.html', data, function(err, output) {
+//   if (err) throw err;
+//   console.log(output);
+// } );
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
@@ -31,21 +34,21 @@ app.use(volleyball);
 //     next();
 // })
 
-// app.use('/special/', function (req,res,next){
-//   console.log('special area');
+// app.use('/tweet/', function (req,res,next){
+//   console.log();
 //   next();
 
 // })
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-});
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// });
 
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-const fillIns = {title: 'Hall of Fame', people: people, copywrite: "Jodie&Stef"};
-app.get('/halloffame', function (req, res) {
-  res.render('index.html', fillIns);
-});
+// const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// const fillIns = {title: 'Hall of Fame', people: people, copywrite: "Jodie&Stef"};
+// app.get('/halloffame', function (req, res) {
+//   res.render('index.html', fillIns);
+// });
 
 
 
